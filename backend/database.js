fb64341a800300a8c642cad4da43fac70e8ff6b5
ATHISH RASWANTH.D <131686223+Athish-Raswanth-D@users.noreply.github.com>
2025-06-16 22:54:12 +0530

    database.js

--- backend/database.js
+++ backend/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Ar@26112004',
-  database: 'fliq'
+  host: '',
+  user: '',
+  password: '',
+  database: ''
 });
 
 connection.connect(err => {
